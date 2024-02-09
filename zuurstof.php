2aadbc56b1af4d8b0c7573d0824ec9422a5c9294
SWCL0 <stan.w.c@icloud.com>
2024-02-09 11:23:49 +0100

    update zuurtstof.php
    opvraag gegevebs gebruiker en berekenen

--- zuurstof.php
+++ zuurstof.php
@@ -1,5 +1,15 @@
 <?php
 
+echo "Cilinderinhoud (liter): " . PHP_EOL;
+$cilinderinhoud = readline();
 
+echo "Druk op de manometer (bar): " . PHP_EOL;
+$bar = readline();
+
+echo "Benodigd aantal liter zuurstof per minuut: " . PHP_EOL;
+$zuurtstof = readline();
+
+$output = ($cilinderinhoud * $bar) / $zuurtstof;
+echo "De patiënt kan de fles nog $output minuten gebruiken!";
 
 ?>
