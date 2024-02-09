e7c9745b03172819cba064a99117de1d1f84d9d7
SWCL0 <stan.w.c@icloud.com>
2024-02-09 11:24:37 +0100

    update zuurstof.php
    toevoeging commentaar regels

--- zuurstof.php
+++ zuurstof.php
@@ -1,14 +1,18 @@
 <?php
 
+// opvraag cilinderinhoud
 echo "Cilinderinhoud (liter): " . PHP_EOL;
 $cilinderinhoud = readline();
 
+// opvraag bar
 echo "Druk op de manometer (bar): " . PHP_EOL;
 $bar = readline();
 
+// opvraag zuurtstof
 echo "Benodigd aantal liter zuurstof per minuut: " . PHP_EOL;
 $zuurtstof = readline();
 
+// berekenen uitkomst
 $output = ($cilinderinhoud * $bar) / $zuurtstof;
 echo "De patiënt kan de fles nog $output minuten gebruiken!";
 
